1fd5f572405eb29c9d306cb505eafb1eeb3da3bd
shirayuki <shirayuki@46ki75.com>
2024-03-24 18:01:37 +0900

    feat(ImageWithModal): display fallback during image loading

--- src/components/typography/ImageWithModal.tsx
+++ src/components/typography/ImageWithModal.tsx
@@ -2,7 +2,7 @@
 'use client'
 
 import React, { memo, useEffect, useState } from 'react'
-import { type SerializedStyles, css } from '@emotion/react'
+import { type SerializedStyles, css, keyframes } from '@emotion/react'
 import { createPortal } from 'react-dom'
 
 const imageStyle = css`
@@ -10,6 +10,13 @@ const imageStyle = css`
 
   user-select: none;
   cursor: zoom-in;
+
+  animation-name: ${keyframes`
+    from { opacity: 0; };
+    to { opacity: 1; };
+  `};
+  animation-fill-mode: both;
+  animation-duration: 0.3s;
 `
 
 const modalStyle = (isModalShown: boolean): SerializedStyles => css`
@@ -64,16 +71,41 @@ export const ImageWithModal = memo(
       }
     }, [])
 
+    const [base64ImageURI, setBase64ImageURI] = useState<string | null>(null)
+
+    useEffect(() => {
+      void (async () => {
+        const response = await fetch(src)
+        const blob = await response.blob()
+
+        const reader = new FileReader()
+        reader.readAsDataURL(blob)
+        reader.onloadend = function () {
+          const base64data = reader.result
+
+          if (typeof base64data === 'string') {
+            setBase64ImageURI(base64data)
+          } else {
+            setBase64ImageURI(null)
+          }
+        }
+      })()
+    }, [src])
+
     return (
       <>
-        <img
-          css={imageStyle}
-          src={src}
-          alt={alt ?? ''}
-          onClick={() => {
-            setIsModalShown(true)
-          }}
-        />
+        {base64ImageURI === null ? (
+          <>LOADING</>
+        ) : (
+          <img
+            css={imageStyle}
+            src={base64ImageURI}
+            alt={alt ?? ''}
+            onClick={() => {
+              setIsModalShown(true)
+            }}
+          />
+        )}
         {createPortal(
           <div
             css={modalStyle(isModalShown)}
@@ -81,7 +113,9 @@ export const ImageWithModal = memo(
               setIsModalShown(false)
             }}
           >
-            <img src={src} alt={alt ?? ''} />
+            {base64ImageURI != null && (
+              <img src={base64ImageURI} alt={alt ?? ''} />
+            )}
           </div>,
           document.body
         )}
