7ce70dbe072b143d8e83ad29d653dd39127a8d7f
shirayuki <shirayuki@46ki75.com>
2024-03-24 16:25:04 +0900

    feat(toggle): enhance component with animation

--- src/components/typography/Toggle.stories.tsx
+++ src/components/typography/Toggle.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
 import { Toggle } from './Toggle'
+import { CodeBlock } from '../code/CodeBlock'
 
 const meta: Meta<typeof Toggle> = {
   component: Toggle,
@@ -20,6 +21,43 @@ export const Primary: Story = {
         Equation block objects are represented as children of paragraph blocks.
         They are nested within a rich text object and contain the following
         information within the equation property:
+      </div>
+    )
+  }
+}
+const code = `
+use reqwest::Error;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Post {
+    #[serde(rename = "userId")]
+    user_id: u32,
+    id: u32,
+    title: String,
+    body: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let url = "https://jsonplaceholder.typicode.com/posts";
+    let response = reqwest::get(url).await?;
+    let posts: Vec<Post> = response.json().await?;
+
+    for post in posts.iter().take(5) {
+        println!("ID: {}, Title: {}", post.id, post.title);
+    }
+
+    Ok(())
+}
+`
+
+export const Code: Story = {
+  args: {
+    summary: 'Code Snippets',
+    children: (
+      <div>
+        <CodeBlock code={code} language={'rust'} />
       </div>
     )
   }
--- src/components/typography/Toggle.tsx
+++ src/components/typography/Toggle.tsx
@@ -1,7 +1,7 @@
 /** @jsxImportSource @emotion/react */
 'use client'
 
-import React, { useState, type ReactNode, useRef, useEffect, memo } from 'react'
+import React, { useState, type ReactNode, memo } from 'react'
 import { type SerializedStyles, css, keyframes } from '@emotion/react'
 
 const detailsStyle = css``
@@ -17,30 +17,33 @@ const summaryStyle = (isOpen: boolean): SerializedStyles => css`
   cursor: pointer;
   user-select: none;
   padding: 0.75rem;
-  border: solid 1px rgba(0, 0, 0, 0.2);
+  border: solid 1px rgba(0, 0, 0, 0.25);
 `
 
-const childrenStyle = css`
+const childrenStyle = (isOpen: boolean): SerializedStyles => css`
   padding: 0.75rem;
-  border: solid 1px rgba(0, 0, 0, 0.2);
+  border: solid 1px rgba(0, 0, 0, 0.25);
   border-top: none;
 
   border-radius: 0 0 0.25rem 0.25rem;
 
-  transition: all 0.4s;
+  transition: all 0.2s ${isOpen ? '0s' : '0.2s'};
 
-  animation-name: ${keyframes`
-    from { opacity: 0; };
-    to { opacity: 1; };
-  `};
-  animation-duration: 0.2s;
-  animation-fill-mode: both;
+  transform-origin: 50% 0%;
+  transform: scaleY(${isOpen ? 1 : 0});
+  height: ${isOpen ? '100%' : 0};
+
+  div {
+    transition: opacity 0.2s ${isOpen ? '0.2s' : '0s'};
+    opacity: ${isOpen ? 1 : 0};
+  }
 `
 
-const foldButtonStyle = css`
+const foldButtonStyle = (isOpen: boolean): SerializedStyles => css`
   padding: 0.5rem 0;
 
-  opacity: 0.7;
+  opacity: ${isOpen ? 0.7 : 0};
+  transition: all 0.2s;
 
   display: flex;
   flex-direction: row;
@@ -49,13 +52,6 @@ const foldButtonStyle = css`
   gap: 0.25rem;
 
   cursor: pointer;
-
-  animation-name: ${keyframes`
-    from { opacity: 0; };
-    to { opacity: 1; };
-  `};
-  animation-duration: 0.2s;
-  animation-fill-mode: both;
 `
 
 interface ToggleProps {
@@ -76,91 +72,71 @@ interface ToggleProps {
 
 export const Toggle = memo(
   ({ summary, children }: ToggleProps): React.JSX.Element => {
-    const detailsRef = useRef<HTMLDetailsElement>(null)
     const [isOpen, setIsOpen] = useState(false)
 
-    useEffect(() => {
-      const detailsElement = detailsRef.current
-      if (detailsElement == null) return
-
-      const observerCallback = (mutationsList: any): void => {
-        for (const mutation of mutationsList) {
-          if (
-            mutation.type === 'attributes' &&
-            mutation.attributeName === 'open'
-          ) {
-            setIsOpen(detailsElement.open)
-          }
-        }
-      }
-
-      const observer = new MutationObserver(observerCallback)
-      const config = { attributes: true }
-
-      observer.observe(detailsElement, config)
-
-      return () => {
-        observer.disconnect()
-      }
-    }, [])
-
-    const close = (): void => {
-      if (detailsRef.current != null) detailsRef.current.open = false
-    }
-
     return (
-      <details css={detailsStyle} ref={detailsRef}>
-        <summary css={summaryStyle(isOpen)}>
-          <div>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width={12}
-              height={12}
-              opacity={0.6}
-              viewBox='0 0 352 512'
-              css={css`
-                margin-right: 0.5rem;
-                transition: all 0.2s;
-                transform: rotateZ(${isOpen ? '180deg' : '45deg'});
-              `}
-            >
-              <path
-                d='M242.7 256l100.1-100.1c12.3-12.3 12.3-32.2 0-44.5l-22.2-22.2c-12.3-12.3-32.2-12.3-44.5 0L176 189.3 75.9 89.2c-12.3-12.3-32.2-12.3-44.5 0L9.2 111.5c-12.3 12.3-12.3 32.2 0 44.5L109.3 256 9.2 356.1c-12.3 12.3-12.3 32.2 0 44.5l22.2 22.2c12.3 12.3 32.2 12.3 44.5 0L176 322.7l100.1 100.1c12.3 12.3 32.2 12.3 44.5 0l22.2-22.2c12.3-12.3 12.3-32.2 0-44.5L242.7 256z'
-                fill='darkgreen'
-              />
-            </svg>
-            <span>{summary}</span>
-          </div>
-
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width={12}
-            viewBox='0 0 448 512'
-            css={css`
-              transform: rotateX(${isOpen ? '180deg' : '0deg'});
-              transition: all 0.2s;
-            `}
-          >
-            <path d='M207 381.5L12.7 187.1c-9.4-9.4-9.4-24.6 0-33.9l22.7-22.7c9.4-9.4 24.5-9.4 33.9 0L224 284.5l154.7-154c9.4-9.3 24.5-9.3 33.9 0l22.7 22.7c9.4 9.4 9.4 24.6 0 33.9L241 381.5c-9.4 9.4-24.6 9.4-33.9 0z' />
-          </svg>
-        </summary>
-        {isOpen && (
-          <>
-            <div css={childrenStyle}>{children}</div>
-            <div css={foldButtonStyle} onClick={close}>
+      <>
+        <details
+          css={detailsStyle}
+          onClick={() => {
+            setIsOpen(!isOpen)
+          }}
+        >
+          <summary css={summaryStyle(isOpen)}>
+            <div>
               <svg
                 xmlns='http://www.w3.org/2000/svg'
                 width={12}
                 height={12}
+                opacity={0.6}
                 viewBox='0 0 352 512'
+                css={css`
+                  margin-right: 0.5rem;
+                  transition: all 0.2s;
+                  transform: rotateZ(${isOpen ? '180deg' : '45deg'});
+                `}
               >
-                <path d='M242.7 256l100.1-100.1c12.3-12.3 12.3-32.2 0-44.5l-22.2-22.2c-12.3-12.3-32.2-12.3-44.5 0L176 189.3 75.9 89.2c-12.3-12.3-32.2-12.3-44.5 0L9.2 111.5c-12.3 12.3-12.3 32.2 0 44.5L109.3 256 9.2 356.1c-12.3 12.3-12.3 32.2 0 44.5l22.2 22.2c12.3 12.3 32.2 12.3 44.5 0L176 322.7l100.1 100.1c12.3 12.3 32.2 12.3 44.5 0l22.2-22.2c12.3-12.3 12.3-32.2 0-44.5L242.7 256z' />
+                <path
+                  d='M242.7 256l100.1-100.1c12.3-12.3 12.3-32.2 0-44.5l-22.2-22.2c-12.3-12.3-32.2-12.3-44.5 0L176 189.3 75.9 89.2c-12.3-12.3-32.2-12.3-44.5 0L9.2 111.5c-12.3 12.3-12.3 32.2 0 44.5L109.3 256 9.2 356.1c-12.3 12.3-12.3 32.2 0 44.5l22.2 22.2c12.3 12.3 32.2 12.3 44.5 0L176 322.7l100.1 100.1c12.3 12.3 32.2 12.3 44.5 0l22.2-22.2c12.3-12.3 12.3-32.2 0-44.5L242.7 256z'
+                  fill='darkgreen'
+                />
               </svg>
-              <span>FOLD</span>
+              <span>{summary}</span>
             </div>
-          </>
-        )}
-      </details>
+
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width={12}
+              viewBox='0 0 448 512'
+              css={css`
+                transform: rotateX(${isOpen ? '180deg' : '0deg'});
+                transition: all 0.2s;
+              `}
+            >
+              <path d='M207 381.5L12.7 187.1c-9.4-9.4-9.4-24.6 0-33.9l22.7-22.7c9.4-9.4 24.5-9.4 33.9 0L224 284.5l154.7-154c9.4-9.3 24.5-9.3 33.9 0l22.7 22.7c9.4 9.4 9.4 24.6 0 33.9L241 381.5c-9.4 9.4-24.6 9.4-33.9 0z' />
+            </svg>
+          </summary>
+        </details>
+        <div css={childrenStyle(isOpen)}>
+          <div>{children}</div>
+        </div>
+        <div
+          css={foldButtonStyle(isOpen)}
+          onClick={() => {
+            setIsOpen(false)
+          }}
+        >
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width={12}
+            height={12}
+            viewBox='0 0 352 512'
+          >
+            <path d='M242.7 256l100.1-100.1c12.3-12.3 12.3-32.2 0-44.5l-22.2-22.2c-12.3-12.3-32.2-12.3-44.5 0L176 189.3 75.9 89.2c-12.3-12.3-32.2-12.3-44.5 0L9.2 111.5c-12.3 12.3-12.3 32.2 0 44.5L109.3 256 9.2 356.1c-12.3 12.3-12.3 32.2 0 44.5l22.2 22.2c12.3 12.3 32.2 12.3 44.5 0L176 322.7l100.1 100.1c12.3 12.3 32.2 12.3 44.5 0l22.2-22.2c12.3-12.3 12.3-32.2 0-44.5L242.7 256z' />
+          </svg>
+          <span>FOLD</span>
+        </div>
+      </>
     )
   }
 )
